4c8fefa0dcd2762f31339fab5547f02f65c36dba
unknown <eprem9@gmail.com>
2026-05-14 15:11:57 +0400

    Add default string length for database schema
    - Added a default string length of 191 characters in AppServiceProvider to prevent issues with string length in database migrations.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Review;
 use App\Observers\ReviewObserver;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Review::observe(ReviewObserver::class);
+        Schema::defaultStringLength(191);
     }
 }
